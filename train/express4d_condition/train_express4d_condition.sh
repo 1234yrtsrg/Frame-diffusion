d7b39c76c6f8dc526d5318d4a726f6d6d2aab380
lzl <1520804066@qq.com>
2026-06-17 11:55:15 +0800

    更新 train_express4d_condition.sh 脚本，调整 CUDA 设备和批处理大小

--- train/express4d_condition/train_express4d_condition.sh
+++ train/express4d_condition/train_express4d_condition.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0,1,2,3}"
+export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0,7}"
 export NCCL_DEBUG="${NCCL_DEBUG:-WARN}"
 
 python train/express4d_condition/train_express4d_condition.py \
   --config CSDI/config/express4d_condition.yaml \
   --device "${DEVICE:-cuda:0}" \
-  --batch_size "${BATCH_SIZE:-1024}" \
+  --batch_size "${BATCH_SIZE:-512}" \
   --max_train_steps "${MAX_TRAIN_STEPS:-50000}" \
   --save_interval_steps "${SAVE_INTERVAL_STEPS:-10000}" \
   --data_parallel \
