b2becd0c1f8d9c6e5623092389831713fc1a5769
lzl <1520804066@qq.com>
2026-06-17 12:22:48 +0800

    更新 train_express4d_condition.sh 脚本，将批处理大小从 512 调整为 2048

--- train/express4d_condition/train_express4d_condition.sh
+++ train/express4d_condition/train_express4d_condition.sh
@@ -7,7 +7,7 @@ export NCCL_DEBUG="${NCCL_DEBUG:-WARN}"
 python train/express4d_condition/train_express4d_condition.py \
   --config CSDI/config/express4d_condition.yaml \
   --device "${DEVICE:-cuda:0}" \
-  --batch_size "${BATCH_SIZE:-512}" \
+  --batch_size "${BATCH_SIZE:-2048}" \
   --max_train_steps "${MAX_TRAIN_STEPS:-50000}" \
   --save_interval_steps "${SAVE_INTERVAL_STEPS:-10000}" \
   --data_parallel \
