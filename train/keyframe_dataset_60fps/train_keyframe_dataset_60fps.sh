2a309bc9ffab229f20499ea2564c56bf9395efda
lzl <1520804066@qq.com>
2026-06-18 08:30:27 +0800

    更新 train_keyframe_dataset_60fps.sh 脚本，调整 CUDA 设备数量和批处理大小

--- train/keyframe_dataset_60fps/train_keyframe_dataset_60fps.sh
+++ train/keyframe_dataset_60fps/train_keyframe_dataset_60fps.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0,1}"
+export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0,1,2,3,4,5,6,7}"
 export NCCL_DEBUG="${NCCL_DEBUG:-WARN}"
 
 python train/keyframe_dataset_60fps/train_keyframe_dataset_60fps.py \
   --config CSDI/config/keyframe_dataset_60fps.yaml \
   --device "${DEVICE:-cuda:0}" \
-  --batch_size "${BATCH_SIZE:-1024}" \
+  --batch_size "${BATCH_SIZE:-4096}" \
   --max_train_steps "${MAX_TRAIN_STEPS:-50000}" \
   --save_interval_steps "${SAVE_INTERVAL_STEPS:-10000}" \
   --dataset_root "${DATASET_ROOT:-dataset/keyframe_dataset_60fps}" \
